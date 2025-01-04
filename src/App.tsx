43aeb83390d0e5704d56c4df7631d2a25bbf700a
Vactor0911 <vactor0911@gmail.com>
2025-01-05 02:42:08 +0900

    브라우저 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router"
+import Header from "./components/Header"
+import { Reservation } from "./pages"
+
 function App() {
   return (
-    <>
-      <h1>FabLab 예약 시스템</h1>
-      <p>메인 화면</p>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/reservation" />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/reservation2" element={<h1>asd</h1>} />
+        <Route path="*" element={<Navigate to="/reservation" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
