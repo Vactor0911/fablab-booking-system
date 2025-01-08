6346496234e2bfdf9d69ec642ec2640bffdd3c6f
Vactor0911 <vactor0911@gmail.com>
2025-01-08 17:06:57 +0900

    공지사항 배너 말줄임 기능 추가

--- src/components/NoticeBanner.tsx
+++ src/components/NoticeBanner.tsx
@@ -13,20 +13,40 @@ const NoticeBanner = (props: NoticeBannerProps) => {
         padding: "3px 0",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "space-between",
-        alignItems: "center",
         backgroundColor: "#c90f0f",
         color: "white",
         cursor: "pointer",
         "& > span": {
-            width: "100%",
-            height: "3px",
-            backgroundColor: "white",
-        }
+          width: "100%",
+          height: "3px",
+          backgroundColor: "white",
+        },
       }}
     >
       <span />
-      <h2>{message + (moreCount ? ` +${moreCount}` : "")}</h2>
+      <div
+        className="text-wrapper"
+        css={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+          width: "100%",
+          height: "100%",
+          padding: "0 20px",
+        }}
+      >
+        <h2
+          css={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {message}
+        </h2>
+        {moreCount && <h2>{`+${moreCount}`}</h2>}
+      </div>
       <span />
     </div>
   );
