5df8f9d88a2a462fb80f81a3beee419ee5b4ca5d
Vactor0911 <vactor0911@gmail.com>
2025-01-08 16:54:17 +0900

    좌석 예약 페이지에 공지사항 배너 추가

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,3 +1,4 @@
+import NoticeBanner from "../components/NoticeBanner";
 import SeatLayout from "../components/SeatLayout";
 
 const Reservation = () => {
@@ -6,13 +7,22 @@ const Reservation = () => {
       css={{
         display: "flex",
         flexDirection: "column",
-        height: "calc(100vh - 70px)",
+        height: "calc(100vh - 80px)",
+        paddingTop: "10px",
       }}
     >
-      <div className="notice-banner-container">
-
-      </div>
-      <div className="seat-layout-root" css={{ padding: "40px", border: "1px solid black", width: "100%", height: "100%" }}>
+      <NoticeBanner
+        message="공지사항 어쩌구 저쩌구... 대충 내일까지 이러 저러한 이유로 팹랩 못쓴단 안내문"
+        moreCount={3}
+      />
+      <div
+        className="seat-layout-root"
+        css={{
+          padding: "10px 40px",
+          width: "100%",
+          height: "100%",
+        }}
+      >
         <SeatLayout />
       </div>
     </div>
