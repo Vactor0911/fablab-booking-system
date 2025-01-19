e8829e8749f7a926bb80c29545c9f7990c1127d2
Vactor0911 <vactor0911@gmail.com>
2025-01-19 18:49:24 +0900

    헤더 디자인 변경 및 드로어 메뉴 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,14 @@ import {
   AppBar,
   Button,
   IconButton,
+  List,
+  ListItemButton,
   ListItemIcon,
   ListItemText,
   Menu,
   MenuItem,
   Stack,
+  SwipeableDrawer,
   ThemeProvider,
   Toolbar,
   Tooltip,
@@ -18,7 +21,6 @@ import { useCallback, useRef, useState } from "react";
 import { useAtom, useAtomValue } from "jotai";
 import { isDarkModeAtom, loginStateAtom } from "../states";
 
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
@@ -26,6 +28,8 @@ import ExitToAppRoundedIcon from "@mui/icons-material/ExitToAppRounded";
 import DarkModeRoundedIcon from "@mui/icons-material/DarkMode";
 import LightModeRoundedIcon from "@mui/icons-material/LightMode";
 import HelpOutlineRoundedIcon from "@mui/icons-material/HelpOutlineRounded";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 // Link 요소 CSS
 const LinkCss = {
@@ -66,13 +70,36 @@ const Header = () => {
     );
   };
 
+  // 드로어 네비게이션 버튼
+  const DrawerNavButton = (text: string, to: string) => {
+    return (
+      <ListItemButton
+        onClick={() => handleDrawerNavButtonClick(to)}
+        sx={{
+          backgroundColor: location.pathname === to ? "white" : "transparent",
+          color:
+            location.pathname === to ? theme.palette.primary.main : "white",
+        }}
+      >
+        <ListItemText
+          primary={text}
+          slotProps={{ primary: { fontWeight: "bold" } }}
+        />
+      </ListItemButton>
+    );
+  };
+
   // 내 계정 드롭다운 메뉴
   const anchorAccountMenuElem = useRef<HTMLButtonElement>(null); // 내 계정 메뉴 버튼 엘리먼트
   const [accountMenuOpen, setAccountMenuOpen] = useState(false); // 내 계정 메뉴 열림 상태
   const loginState = useAtomValue(loginStateAtom); // 로그인 상태
 
+  // 드로어 메뉴
+  const anchorDrawerElem = useRef<HTMLButtonElement>(null); // 드로어 메뉴 버튼 엘리먼트
+  const [drawerOpen, setDrawerOpen] = useState(false); // 드로어 메뉴 열림 상태
+
   // 내 계정 버튼 클릭
-  const handleAccountClick = useCallback(() => {
+  const handleAccountButtonClick = useCallback(() => {
     if (!loginState) {
       navigate("/login");
       return;
@@ -84,6 +111,30 @@ const Header = () => {
   const handleAccountMenuClose = useCallback(() => {
     setAccountMenuOpen(false);
   }, []);
+
+  // 드로어 메뉴 열기
+  const handleDrawerOpen = useCallback(() => {
+    setDrawerOpen(true);
+  }, []);
+
+  // 드로어 메뉴 버튼 클릭
+  const handleDrawerButtonClick = useCallback(() => {
+    handleDrawerOpen();
+  }, [handleDrawerOpen]);
+
+  // 드로어 메뉴 닫기
+  const handleDrawerClose = useCallback(() => {
+    setDrawerOpen(false);
+  }, []);
+
+  // 드로어 메뉴 네비게이션 버튼 클릭
+  const handleDrawerNavButtonClick = useCallback(
+    (to: string) => {
+      handleDrawerClose();
+      navigate(to);
+    },
+    [handleDrawerClose, navigate]
+  );
 
   // 다크모드
   const [isDarkMode, setIsDarkMode] = useAtom(isDarkModeAtom); // 다크모드 상태
@@ -105,18 +156,11 @@ const Header = () => {
               : theme.palette.primary.main,
           position: location.pathname === "/" ? "absolute" : "relative",
           zIndex: 100,
-          [theme.breakpoints.only("xs")]: {
-            backgroundColor: "transparent",
-            position: "relative",
-          },
         }}
       >
         <Toolbar
           sx={{
             height: "80px",
-            [theme.breakpoints.only("xs")]: {
-              height: "60px",
-            },
             justifyContent: "space-between",
           }}
         >
@@ -127,7 +171,6 @@ const Header = () => {
               fontWeight="bold"
               sx={{
                 color: "white",
-                [theme.breakpoints.only("xs")]: { color: "black" },
               }}
             >
               FabLab
@@ -135,44 +178,58 @@ const Header = () => {
           </Link>
 
           {/* 네비게이션 바 */}
-          <Stack
-            direction="row"
-            gap={{
-              xs: 0,
-              sm: 2,
-            }}
-          >
-            {/* PC, 태블릿용 네비게이션 바 */}
-            <Stack direction="row" gap={2} display={{ xs: "none", sm: "flex" }}>
+          <Stack direction="row" gap={2}>
+            <Stack
+              direction="row"
+              gap={2}
+              display={{
+                xs: "none",
+                sm: "flex",
+              }}
+            >
               {NavLinkButton("예약하기", "/reservation")}
               {NavLinkButton("팹랩소개", "/about")}
               {NavLinkButton("공지사항", "/notice")}
             </Stack>
 
-            {/* 모바일용 네비게이션 바 */}
-            <Stack display={{ xs: "flex", sm: "none" }}>
-              <Tooltip title="메인으로">
-                <IconButton onClick={() => navigate("/")}>
-                  <HomeRoundedIcon fontSize="large" />
-                </IconButton>
-              </Tooltip>
-            </Stack>
-
             {/* 내 계정 버튼 */}
             <Stack justifyContent="center">
-              <Tooltip title="내 계정">
+              <Tooltip title={loginState ? "내 계정" : "로그인"}>
                 <IconButton
-                  onClick={handleAccountClick}
+                  onClick={handleAccountButtonClick}
                   ref={anchorAccountMenuElem}
                   sx={{ p: 0 }}
                 >
                   <AccountCircleRoundedIcon
                     fontSize="large"
                     sx={{
-                      [theme.breakpoints.up("sm")]: {
-                        color: "white",
-                        fontSize: "2em",
-                      },
+                      color: "white",
+                      fontSize: "2em",
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+            </Stack>
+
+            {/* 모바일용 햄버거 메뉴 */}
+            <Stack
+              justifyContent="center"
+              display={{
+                xs: "flex",
+                sm: "none",
+              }}
+            >
+              <Tooltip title="메뉴">
+                <IconButton
+                  onClick={handleDrawerButtonClick}
+                  ref={anchorDrawerElem}
+                  sx={{ p: 0 }}
+                >
+                  <MenuRoundedIcon
+                    fontSize="large"
+                    sx={{
+                      color: "white",
+                      fontSize: "2em",
                     }}
                   />
                 </IconButton>
@@ -189,11 +246,11 @@ const Header = () => {
         onClose={handleAccountMenuClose}
         anchorOrigin={{
           vertical: "bottom",
-          horizontal: "right",
+          horizontal: "center",
         }}
         transformOrigin={{
           vertical: "top",
-          horizontal: "right",
+          horizontal: "center",
         }}
         sx={{
           marginTop: "5px",
@@ -258,6 +315,52 @@ const Header = () => {
           <ListItemText>도움말</ListItemText>
         </MenuItem>
       </Menu>
+
+      {/* 드로어 메뉴 */}
+      <SwipeableDrawer
+        anchor="right"
+        open={drawerOpen}
+        onClose={handleDrawerClose}
+        onOpen={handleDrawerOpen}
+      >
+        <Stack
+          width={250}
+          height="100%"
+          sx={{
+            backgroundColor: theme.palette.primary.main,
+          }}
+        >
+          {/* 상단 버튼 */}
+          <Stack
+            direction="row"
+            padding={1}
+            justifyContent="flex-end"
+            boxShadow={2}
+          >
+            <IconButton onClick={handleDrawerClose} sx={{ p: 0 }}>
+              <CloseRoundedIcon
+                sx={{
+                  fontSize: "2em",
+                  color: "white",
+                }}
+              />
+            </IconButton>
+          </Stack>
+
+          {/* 네비게이션 메뉴 */}
+          <List
+            disablePadding
+            sx={{
+              color: "white",
+              fontWeight: "bold",
+            }}
+          >
+            {DrawerNavButton("예약하기", "/reservation")}
+            {DrawerNavButton("팹랩소개", "/about")}
+            {DrawerNavButton("공지사항", "/notice")}
+          </List>
+        </Stack>
+      </SwipeableDrawer>
     </ThemeProvider>
   );
 };
