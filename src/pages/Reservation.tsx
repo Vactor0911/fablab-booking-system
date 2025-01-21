7224bec7c111c2c486e7608df84b436062869d8d
Vactor0911 <vactor0911@gmail.com>
2025-01-21 20:31:28 +0900

    예약하기 페이지 디자인 변경

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,31 +1,31 @@
+import { Box, Stack, ThemeProvider } from "@mui/material";
 import NoticeBanner from "../components/NoticeBanner";
 import SeatLayout from "../components/SeatLayout";
+import { theme } from "../utils";
+import ReservationDialog from "../components/ReservationDialog";
 
 const Reservation = () => {
   return (
-    <div
-      css={{
-        display: "flex",
-        flexDirection: "column",
-        height: "calc(100vh - 80px)",
-        paddingTop: "10px",
-      }}
-    >
-      <NoticeBanner
-        message="공지사항 어쩌구 저쩌구... 대충 내일까지 이러 저러한 이유로 팹랩 못쓴단 안내문"
-        moreCount={3}
-      />
-      <div
-        className="seat-layout-root"
-        css={{
-          padding: "10px 40px",
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <SeatLayout />
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Stack className="page-root">
+        {/* 공지사항 배너 */}
+        <Box width="100%" padding="10px 0">
+          <NoticeBanner
+            message="공지사항 어쩌구 저쩌구... 대충 내일까지 이러 저러한 이유로 팹랩 못쓴단 안내문"
+            moreCount={3}
+          />
+        </Box>
+
+        {/* 좌석 배치도 */}
+        <Stack width="100%" flex={1} sx={{
+          overflowX: "auto",
+        }}>
+          <SeatLayout flex={1} margin="20px 20px" />
+        </Stack>
+      </Stack>
+
+      <ReservationDialog seatName="A1" open={true} onClose={() => {}} />
+    </ThemeProvider>
   );
 };
 
