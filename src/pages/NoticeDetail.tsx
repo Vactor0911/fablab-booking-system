a396cb529451474f54065f5b0d821881ffaf0f7a
Vactor0911 <vactor0911@gmail.com>
2025-01-30 21:20:00 +0900

    버튼 이벤트 리스너 함수 추가

--- src/pages/NoticeDetail.tsx
+++ src/pages/NoticeDetail.tsx
@@ -6,13 +6,82 @@ import {
   Typography,
 } from "@mui/material";
 import { dateFormatter, theme } from "../utils";
-import { Link, useLocation } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 import SectionHeader from "../components/SectionHeader";
 
 import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
+import { useAtomValue } from "jotai";
+import { loginStateAtom, Permission } from "../states";
+import { useCallback, useEffect, useState } from "react";
 
 const NoticeDetail = () => {
   const location = useLocation();
+
+  // 관리자가 아닐 경우 새 공지사항 등록 페이지 접근 불가
+  const navigate = useNavigate();
+  const loginState = useAtomValue(loginStateAtom);
+
+  useEffect(() => {
+    if (location.pathname === "/notice/new") {
+      if (!loginState.isLoggedIn || loginState.permission === Permission.USER) {
+        alert("잘못된 접근입니다.");
+        navigate("/");
+      }
+    }
+  }, [
+    location.pathname,
+    loginState.isLoggedIn,
+    loginState.permission,
+    navigate,
+  ]);
+
+  // 수정 모드
+  const [isEditing, setIsEditing] = useState(false);
+
+  // 제목
+  const [title, setTitle] = useState("");
+  const handleTitleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTitle(e.target.value);
+    },
+    []
+  );
+
+  // 내용
+  const [content, setContent] = useState("");
+  const handleContentChange = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setContent(e.target.value);
+    },
+    []
+  );
+
+  // 등록 버튼 클릭
+  const handlePostButtonClick = useCallback(() => {
+    // TODO: 등록 처리
+  }, []);
+
+  // 수정 버튼 클릭
+  const handleEditButtonClick = useCallback(() => {
+    setIsEditing(true);
+  }, []);
+
+  // 삭제 버튼 클릭
+  const handleDeleteButtonClick = useCallback(() => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      // 삭제 처리
+    }
+  }, []);
+
+  // 수정 취소 버튼 클릭
+  const handleCancelButtonClick = useCallback(() => {
+    setIsEditing(false);
+  }, []);
+
+  // 수정 확인 버튼 클릭
+  const handleConfirmButtonClick = useCallback(() => {
+    setIsEditing(false);
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -57,9 +126,18 @@ const NoticeDetail = () => {
               {/* 제목 */}
               <Stack gap={2} flex={1}>
                 <SectionHeader title="제목" />
-                <Typography variant="subtitle1">
-                  공지사항 제목 어쩌구 저쩌구
-                </Typography>
+                {isEditing ? (
+                  <TextField
+                    placeholder="제목"
+                    fullWidth
+                    value={title}
+                    onChange={handleTitleChange}
+                  />
+                ) : (
+                  <Typography variant="subtitle1">
+                    {title}
+                  </Typography>
+                )}
               </Stack>
 
               {/* 작성자 */}
@@ -103,11 +181,18 @@ const NoticeDetail = () => {
               {/* 내용 */}
               <Stack gap={2} flex={1}>
                 <SectionHeader title="내용" />
-                <TextField
-                  placeholder="내용을 입력해주세요."
-                  multiline
-                  rows={18}
-                />
+
+                {isEditing ? (
+                  <TextField
+                    placeholder="내용"
+                    multiline
+                    rows={18}
+                    value={content}
+                    onChange={handleContentChange}
+                  />
+                ) : (
+                  <Typography component="pre" variant="subtitle1">{content}</Typography>
+                )}
               </Stack>
 
               <Stack
@@ -132,22 +217,10 @@ const NoticeDetail = () => {
                 </Stack>
 
                 {/* 버튼 */}
-                {location.pathname === "/notice/new" && (
-                  // 등록
-                  <Button
-                    variant="outlined"
-                    color="secondary"
-                    fullWidth
-                    sx={{
-                      borderRadius: "30px",
-                    }}
-                  >
-                    <Typography variant="h2">등록</Typography>
-                  </Button>
-                )}
-                {location.pathname !== "/notice/new" && (
-                  <Stack direction="row" gap={3}>
-                    {/* 수정 */}
+                {loginState.isLoggedIn &&
+                  loginState.permission !== Permission.USER &&
+                  (location.pathname === "/notice/new" ? (
+                    // 등록
                     <Button
                       variant="outlined"
                       color="secondary"
@@ -155,23 +228,69 @@ const NoticeDetail = () => {
                       sx={{
                         borderRadius: "30px",
                       }}
+                      onClick={handlePostButtonClick}
                     >
-                      <Typography variant="h2">수정</Typography>
+                      <Typography variant="h2">등록</Typography>
                     </Button>
+                  ) : isEditing ? (
+                    // 수정 모드
+                    <Stack direction="row" gap={3}>
+                      {/* 취소 */}
+                      <Button
+                        variant="outlined"
+                        color="secondary"
+                        fullWidth
+                        sx={{
+                          borderRadius: "30px",
+                        }}
+                        onClick={handleCancelButtonClick}
+                      >
+                        <Typography variant="h2">취소</Typography>
+                      </Button>
 
-                    {/* 삭제 */}
-                    <Button
-                      variant="outlined"
-                      color="error"
-                      fullWidth
-                      sx={{
-                        borderRadius: "30px",
-                      }}
-                    >
-                      <Typography variant="h2">삭제</Typography>
-                    </Button>
-                  </Stack>
-                )}
+                      {/* 확인 */}
+                      <Button
+                        variant="outlined"
+                        color="error"
+                        fullWidth
+                        sx={{
+                          borderRadius: "30px",
+                        }}
+                        onClick={handleConfirmButtonClick}
+                      >
+                        <Typography variant="h2">확인</Typography>
+                      </Button>
+                    </Stack>
+                  ) : (
+                    // 일반 모드
+                    <Stack direction="row" gap={3}>
+                      {/* 수정 */}
+                      <Button
+                        variant="outlined"
+                        color="secondary"
+                        fullWidth
+                        sx={{
+                          borderRadius: "30px",
+                        }}
+                        onClick={handleEditButtonClick}
+                      >
+                        <Typography variant="h2">수정</Typography>
+                      </Button>
+
+                      {/* 삭제 */}
+                      <Button
+                        variant="outlined"
+                        color="error"
+                        fullWidth
+                        sx={{
+                          borderRadius: "30px",
+                        }}
+                        onClick={handleDeleteButtonClick}
+                      >
+                        <Typography variant="h2">삭제</Typography>
+                      </Button>
+                    </Stack>
+                  ))}
               </Stack>
             </Stack>
           </Stack>
