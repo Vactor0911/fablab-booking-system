2fd25f099738f2fe8e4e7978648eba379e8b40a0
Vactor0911 <vactor0911@gmail.com>
2025-02-02 16:31:27 +0900

    로그인 상태 구조 수정

--- src/states/index.ts
+++ src/states/index.ts
@@ -1,25 +1,20 @@
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
 // 다크모드 상태
 export const isDarkModeAtom = atom(false);
 
 // 로그인 상태
 export enum Permission {
-    USER = "user",
-    ADMIN = "admin",
-    SUPER_ADMIN = "superadmin",
+  USER = "user",
+  ADMIN = "admin",
+  SUPER_ADMIN = "superadmin",
 }
 
 export interface LoginState {
-    isLoggedIn: boolean;
-    id: string;
-    userId: string;
-    permission: Permission;
+  isLoggedIn: boolean;
+  userId: string;
+  permission: Permission;
 }
 
-export const loginStateAtom = atom({
-    isLoggedIn: true,
-    id: "123456",
-    userId: "1",
-    permission: Permission.ADMIN,
-} as LoginState);
+export const loginStateAtom = atomWithStorage("FabLabLoginState", {} as LoginState);
